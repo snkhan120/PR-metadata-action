5b999fc429389f930c0e2d2d4423f273b09cc042
snkhan120 <snkhan120@live.com>
2022-01-17 11:51:05 +0500

    functions file is added

---
+++ functions.js
@@ -0,0 +1,24 @@
+// array sort  function
+const functions = {};
+
+functions.sortArray = (array) => {
+    return array.sort((a, b) => a - b);
+}
+
+functions.reverse = (array) => {    
+    return array.reverse();
+}
+
+functions.monogoConnect = (url, dbName) => {
+    return mongoose.connect(url + dbName);
+}
+
+functions.createCollection = (collectionName) => {
+    return mongoose.connection.db.createCollection(collectionName);
+}
+
+functions.insertcollection = (collectionName, data) => {
+    return mongoose.connection.db.collection(collectionName).insert(data);
+}
+
+module.exports = functions;
